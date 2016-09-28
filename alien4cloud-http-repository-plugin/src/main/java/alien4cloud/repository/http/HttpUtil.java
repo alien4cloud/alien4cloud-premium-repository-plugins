79a39700b6734be7eeaedefd2188240088edcaad
Philippe Benoit <philippe.benoit@fastconnect.fr>
2016-09-28 17:10:45 +0200

    Move http and common plugin to Github

--- alien4cloud-http-repository-plugin/src/main/java/alien4cloud/repository/http/HttpUtil.java
+++
@@ -1,9 +0,0 @@
-package alien4cloud.repository.http;
-
-public class HttpUtil {
-
-    public static boolean isHttpURL(String reference) {
-        return reference != null && (reference.startsWith("http://") || reference.startsWith("https://"));
-    }
-
-}
